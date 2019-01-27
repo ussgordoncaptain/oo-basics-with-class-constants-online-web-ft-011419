8bbba50fe8f55847ab7a3663cd0f1cf69cf2ab2c
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-27 06:35:00 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,12 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  GENRES = []
+  def genre=(genre)
+    
+    @genre = genre
+    GENRES.push(genre)
+  end
   def initialize(title)
     @title = title
   end
